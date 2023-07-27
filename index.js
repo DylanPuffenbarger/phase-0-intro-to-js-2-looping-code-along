70eed37080a2bf1d7075adedd930b9fa1b8185db
Dylan Puffenbarger <dylanpuffenbarger@gmail.com>
2023-07-26 20:58:19 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(nameArray){
+    let msgArray = [];
+    for (let i=0; i < nameArray.length; i++){
+        msgArray.push(`Thank you, ${nameArray[i]}, for the wonderful surprise gift!`);
+    }
+    return msgArray;
+}
+
+function countDown(n){
+    let count = n;
+    while (count >= 0){
+        console.log(count--);
+    }
+}
